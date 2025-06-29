a72ff4425c5d9637f7f9ec4606d7fc5a4dd75952
Alexey Anikin <anikin@devexperts.com>
2025-06-29 15:54:00 +0300

    created Button component

---
+++ src/ui-kit/components/button/button.component.tsx
@@ -0,0 +1,13 @@
+import { memo, useMemo, type JSX } from "react";
+import cn from 'classnames';
+
+import css from './button.module.css';
+
+type ButtonProps = JSX.IntrinsicElements['button'];
+
+export const Button = memo<ButtonProps>((props) => {
+    const className = useMemo(() => cn(css.container, props.className), [props.className]);
+    return <button {...props} className={className} />
+})
+
+Button.displayName = 'Button';
