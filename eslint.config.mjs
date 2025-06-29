4b1eb936a2f3f898de96e697118b243ebcd8b481
Alexey Anikin <anikin@devexperts.com>
2025-06-29 13:51:13 +0300

    project can be builded with vite

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,30 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
+	globalIgnores(['dist']),
 	{
 		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		plugins: { js },
-		extends: ['js/recommended'],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			pluginReact.configs.flat.recommended,
+			reactHooks.configs['recommended-latest'],
+			reactRefresh.configs.vite,
+		],
 		languageOptions: {
+            ecmaVersion: 2020,
 			globals: {
 				...globals.browser,
 				...globals.node,
 			},
 		},
 	},
-	tseslint.configs.recommended,
-	pluginReact.configs.flat.recommended,
 	{
 		rules: {
 			// suppress errors for missing 'import React' in files
