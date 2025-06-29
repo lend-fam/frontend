9216b35ddb23f1f2f12acd1631917f7e63035d15
Alexey Anikin <anikin@devexperts.com>
2025-06-29 13:25:45 +0300

    Eslint library installed

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		plugins: { js },
+		extends: ['js/recommended'],
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		rules: {
+			// suppress errors for missing 'import React' in files
+			'react/react-in-jsx-scope': 'off',
+			// allow jsx syntax in js files (for next.js project)
+			'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }], //should add ".ts" if typescript project
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+]);
