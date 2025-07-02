56287c533717bdc2556f6140b07858cc9ed211f3
Alexey Anikin <anikin@devexperts.com>
2025-07-02 18:15:17 +0300

    added useTheme hook

---
+++ src/ui-kit/hooks/use-theme.hook.ts
@@ -0,0 +1,13 @@
+import { useMemo } from 'react';
+import cn from 'classnames';
+
+export const useTheme = <T extends Record<string, string>>(cssTheme: T, propsTheme?: Partial<T>): T =>
+	useMemo(() => {
+		if (propsTheme) {
+			return Object.keys(propsTheme).reduce(
+				(acc, item) => ({ ...acc, [item]: cn(cssTheme[item], item) }),
+				cssTheme,
+			);
+		}
+		return cssTheme;
+	}, [cssTheme, propsTheme]);
