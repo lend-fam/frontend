fe20baa9eb9209b32aa891c013edce8cb357ed87
nniel-ape <noreply@github.com>
2025-07-12 18:45:09 +0300

    feat: make AssetsColumn component dynamic with value prop
    - Accept value prop to display dynamic asset names
    - Replace hardcoded 'ApeCoin' with prop-based rendering
    - Add fallback to 'Unknown Asset' when value is not provided

--- src/ui-kit/components/table/columns/assets-column/assets-column.component.tsx
+++ src/ui-kit/components/table/columns/assets-column/assets-column.component.tsx
@@ -4,16 +4,15 @@ import type { TableColumnRendererProps } from '../../table.component';
 import { Icon } from '../../../icon/icon.component';
 
 import css from './assets-column.module.css';
-
 import coinIcon from '../../../../../assets/svg/coin.svg';
 
 export const AssetsColumn = memo<TableColumnRendererProps>((props) => {
-	const { align, className, style } = props;
+	const { align, className, style, value } = props;
 
 	return (
 		<TableColumn align={align} className={className} style={style}>
 			<Icon src={coinIcon} className={css.icon} />
-			<span>ApeCoin</span>
+			<span>{value || 'Unknown Asset'}</span>
 		</TableColumn>
 	);
 });
